7f5c46853b156a4b455eb29c114dbdbfc23c7b53
cre8ivesolutions <tammerz78@gmail.com>
2022-07-23 10:39:18 -0500

    first

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+
+// // CONFIGURATION
+// require('dotenv').config()
+// const PORT = process.env.PORT
+// console.log(PORT)
+
+// DEPENDENCIES
+// const express = require('express')
+
+// CONFIGURATION
+require('dotenv').config()
+const PORT = process.env.PORT
+const app = express()
+
+// ROUTES
+app.get('/', (req, res) => {
+  res.send('Welcome to an Awesome App about Breads!')
+})
+
+// LISTEN
+app.listen(PORT, () => {
+  console.log('listening on port', PORT);
+})
